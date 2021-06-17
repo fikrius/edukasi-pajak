8751b5d507b185382465e7421d8ececf9f132cf1
fikrius <fikrius@gmail.com>
2021-06-17 22:37:24 +0700

    defer js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1127,14 +1127,14 @@
 
     <script src="{{ asset('/js/vendor/modernizr-3.5.0.min.js') }}"></script>
     <!-- Jquery, Popper, Bootstrap -->
-    <script src="{{ asset('/js/vendor/jquery-1.12.4.min.js') }}"></script>
+    <script defer src="{{ asset('/js/vendor/jquery-1.12.4.min.js') }}"></script>
     <script src="{{ asset('/js/popper.min.js') }}"></script>
-    <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+    <script defer src="{{ asset('/js/bootstrap.min.js') }}"></script>
     <!-- Jquery Mobile Menu -->
     <script src="{{ asset('/js/jquery.slicknav.min.js') }}"></script>
 
     <!-- Jquery Slick , Owl-Carousel Plugins -->
-    <script src="{{ asset('/js/owl.carousel.min.js') }}"></script>
+    <script defer src="{{ asset('/js/owl.carousel.min.js') }}"></script>
     <script src="{{ asset('/js/slick.min.js') }}"></script>
     <!-- One Page, Animated-HeadLin -->
     <script src="{{ asset('/js/wow.min.js') }}"></script>
